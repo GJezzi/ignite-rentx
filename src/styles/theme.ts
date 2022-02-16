25c3c1d764b2fa3519bd1e22a47e4280b7216240
Gianfranco Jezzi <gianfranco.jezzi@mesainc.com.br>
2022-02-16 09:06:19 -0300

    Chapter III - Consumindo API - Criação do Header da Home

---
+++ src/styles/theme.ts
@@ -0,0 +1,34 @@
+export default {
+    colors: {
+        header: '#1B1B1F',
+
+        background_primary: '#F4F5F6',
+        background_secondary: '#E5E5E5',
+        
+        line: '#EBEBF0',
+
+        main: '#DC1637',
+        main_light: '#FDEDEF',
+        success: '#03B352',
+        green_light: '#DAF3E5', 
+
+        white: '#FFFFFF',
+        red_text: '#FF99AA',
+
+        title: '#47474D',
+        text: '#7A7A80',
+        text_detail: '#AEAEB3',
+
+        hover_black: '#DEDEE3',
+        
+        shape_dark: '#29292E', 
+        shape: '#E1E1E8' ,
+    },
+    fonts: {
+        inter_400: 'Inter_400Regular', 
+        inter_500: 'Inter_500Medium',
+        arch_400: 'Archivo_400Regular', 
+        arch_500: 'Archivo_500Medium', 
+        arch_600:'Archivo_600SemiBold'
+    }
+}
